4bfef3f09907b584a6e184a920f80632caa7a009
Thomas Barregren <thomas@kntnt.com>
2021-02-19 16:01:22 +0100

    Improved.

--- includes/kntnt-podcast-player.php
+++ includes/kntnt-podcast-player.php
@@ -1,9 +1,9 @@
 <div class="kntnt-podcast-player">
-    <div>
+    <div class="kntnt-podcast-player-player">
 		<?php echo wp_audio_shortcode( [ 'src' => $src, 'preload' => $preload, 'autoplay' => $autoplay, 'loop' => $loop ], $content ); ?>
     </div>
 	<?php if ( $badges ): ?>
-        <div>
+        <div class="kntnt-podcast-player-badges">
 			<?php foreach ( $badges as $badge ): ?><a href="<?php echo $badge['link']; ?>" title="<?php echo $badge['text']; ?>"><img src="<?php echo $badge['src']; ?>" srcset="<?php echo $badge['srcset']; ?>" alt="<?php echo $badge['text']; ?>"></a><?php endforeach; ?>
         </div>
 	<?php endif; ?>
